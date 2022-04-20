2ab2d8ca2cf0f97bccfecd012ae5ade6c7a19c5d
Uten Yarach <90887089+uten178@users.noreply.github.com>
2022-04-20 14:50:36 +0700

    Add files via upload

---
+++ tfkbnufft/utils/itertools.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def cartesian_product(arrays):
+    # from https://stackoverflow.com/a/11146645/4332585
+    la = len(arrays)
+    dtype = np.result_type(*arrays)
+    arr = np.empty([len(a) for a in arrays] + [la], dtype=dtype)
+    for i, a in enumerate(np.ix_(*arrays)):
+        arr[...,i] = a
+    return arr.reshape(-1, la)
